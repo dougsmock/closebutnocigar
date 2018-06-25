c5fd47cd873623ba9a97f7eb2a2b6a3422fdbc96
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-25 11:32:09 -0400

    Add files via upload

---
+++ closebutnocigar.rb
@@ -0,0 +1,18 @@
+def match_check(ticket1, ticket2)
+  1
+end
+
+
+
+
+
+
+
+
+
+
+#make function
+#takes 2 string as arguments
+#make sure they match
+#
+#function an array of string, indfinite number
