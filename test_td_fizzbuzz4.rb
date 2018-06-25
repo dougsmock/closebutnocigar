c1c3521e7fe60cc367b87d4f61bae226e822ab30
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-25 12:37:47 -0400

    Add files via upload

--- test_td_fizzbuzz4.rb
+++ test_td_fizzbuzz4.rb
@@ -7,4 +7,12 @@ class CloseButNoCigar < Minitest::Test
     assert_equal(Integer, match_check("","").class)
   end
 
+  def test_no_match_returns_0
+    assert_equal(0, match_check('5678','1234'))
+  end
+
+  def test_1_match_returns_1
+    assert_equal(1, match_check("1234", "5555"))
+  end
+
 end
