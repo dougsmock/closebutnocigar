4a2d08d3f4554f024514819986c64c4d8df4f1b0
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-25 12:48:03 -0400

    Add files via upload

--- closebutnocigar.rb
+++ closebutnocigar.rb
@@ -1,3 +1,11 @@
 def match_check(ticket1, ticket2)
-  1
+  ticket1 = ticket1.split('')
+  ticket2 = ticket2.split('')
+  matchcounter = 0
+  ticket1.each_with_index do |num, index|
+    if num == ticket2[index]
+      matchcounter += 1
+    end
+  end
+  matchcounter
 end
