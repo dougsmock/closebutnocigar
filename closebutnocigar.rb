83801c0cacbbb1eebbddb17a4bc1adbff830d7fb
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-26 22:06:42 -0400

    Add files via upload
    About 40 commits through Terminal have not worked. Not that I was able to do much on my own, anyway.

--- closebutnocigar.rb
+++ closebutnocigar.rb
@@ -1,21 +1,4 @@
-def match_check(ticket1, ticket2)
-  ticket1 = ticket1.split('')
-  ticket2 = ticket2.split('')
-  matchcounter = 0
-  ticket1.each_with_index do |num, index|
-    if num == ticket2[index]
-      matchcounter += 1
-    end
-  end
-  matchcounter
-end
-
-def check_list(ticket_list, ticket_winner)
-  number_winners = 0
-  ticket_list.each do |num|
-    if match_check(num, ticket_winner) == 4
-      number_winners += 1
-    end
-  end
-  number_winners
-end
+digitsarr = []
+winner = "1234"
+digitsarr = winner.split('')
+digitsarr.push(winner)
